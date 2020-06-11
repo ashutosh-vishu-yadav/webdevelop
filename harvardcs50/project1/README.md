662f4e327afcdb550f7c842ec8c8ea492aae2e22
rainthecat <vishu10699@gmail.com>
2020-06-11 16:42:00 +0530

    restart the whole project

--- harvardcs50/project1/README.md
+++ harvardcs50/project1/README.md
@@ -1,5 +1,3 @@
 # Project 1
 
 Web Programming with Python and JavaScript
-
-python -c 'import os; print(os.urandom(16))'
--- harvardcs50/project1/application.py
+++ harvardcs50/project1/application.py
@@ -1,20 +1,12 @@
 import os
 
-import requests
-import passwordmeter
-from flask import Flask, session, render_template, jsonify, request
+from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# secret KEY
-app.secret_key
-
-# api
-res = requests.get("https://www.goodreads.com/book/review_counts.json",
-                   params={"key": "KEY", "isbns": "9781632168146"})
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -27,51 +19,8 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-# passwordmeter
-
-# flask login
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-
-@app.route('/signup', methods=['GET'])
-def signup():
-
-    name = request.form.get("name")
-
-    strength, improvements = passwordmeter.test(sys.argv[1])
-   if strength < 0.5:
-        result = "password is very weak."
-        return render_template('signup', improvements)
-
-    elif strength < 1.0:
-        result = "password is weak."
-        return render_template('signup', improvements)
-
-    elif strength > 1.0:
-        return render_template('success.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
-
-
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
+    return "Project 1: TODO"
