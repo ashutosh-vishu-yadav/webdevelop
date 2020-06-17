5e0b15407e7efaa895b4c4b4e3d9980878a2c0b9
rainthecat <vishu10699@gmail.com>
2020-06-17 19:56:40 +0530

    added a half working signup

--- harvardcs50/project1/application.py
+++ harvardcs50/project1/application.py
@@ -1,20 +1,19 @@
 import os
-
-import requests
 import passwordmeter
-from flask import Flask, session, render_template, jsonify, request
+from flask import Flask, session, render_template, jsonify, request, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
+import sys
 app = Flask(__name__)
 
 # secret KEY
-app.secret_key
+app.config['SECRET_KEY'] = '\x84}r\n\xdc\x88\xf8\xf3\x07\x06_*\x9a\xd0\x80\xe88Z\xea\xc6\x83\xbb\xf7\x0e'
+
 
 # api
-res = requests.get("https://www.goodreads.com/book/review_counts.json",
-                   params={"key": "KEY", "isbns": "9781632168146"})
+# res = request.get("https://www.goodreads.com/book/review_counts.json",
+#                  params={"key": "KEY", "isbns": "9781632168146"})
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -30,8 +29,8 @@ db = scoped_session(sessionmaker(bind=engine))
 # passwordmeter
 
 # flask login
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 
 @app.route("/")
@@ -39,22 +38,52 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/signup', methods=['GET'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    result = None
+    if request.method == 'POST':
+        uname = request.form.get("uname")
+        uemail = request.form.get("uemail")
+        upass = request.form.get("upass")
+        urepass = request.form.get("urepass")
+        strength, improvements = passwordmeter.test(upass)
+        if uname is None or uemail is None or upass is None or urepass is None:
+            result = "some fields are vacent,fill the form completely."
+            return render_template('signup.html', result=result)  # , improvements=improvements)
+        elif urepass != upass:
+            result = "password doesn't match, try again."
+            return render_template('signup.html', result=result)  # , improvements=improvements)
+        elif strength < 0.5:
+            result = "password is very weak."
+            return render_template('signup.html', result=result,
+                                   improvements=improvements)
 
-    name = request.form.get("name")
+        elif strength < 0.8:
+            result = "password is weak."
+            return render_template('signup.html', result=result,
+                                   improvements=improvements)
 
-    strength, improvements = passwordmeter.test(sys.argv[1])
-   if strength < 0.5:
-        result = "password is very weak."
-        return render_template('signup', improvements)
+        elif strength > 0.8:
+            return redirect(url_for('login'))
+    return render_template('signup.html')
 
-    elif strength < 1.0:
-        result = "password is weak."
-        return render_template('signup', improvements)
-
-    elif strength > 1.0:
-        return render_template('success.html')
+#
+# @app.route('/signing', methods=['POST'])
+# def signing():
+#     if request.method == 'POST':
+#         name = request.form.get("name")
+#
+#         strength, improvements = passwordmeter.test(sys.argv[1])
+#         if strength < 0.5:
+#             result = "password is very weak."
+#             return render_template('signup.html', improvements=improvements)
+#
+#         elif strength < 1.0:
+#             result = "password is weak."
+#             return render_template('signup.html', improvements=improvements)
+#
+#         elif strength > 1.0:
+#             return render_template('success.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -62,12 +91,7 @@ def login():
     if request.method == 'POST':
         session['username'] = request.form['username']
         return redirect(url_for('index'))
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
+    return render_template('login.html')
 
 
 @app.route('/logout')
